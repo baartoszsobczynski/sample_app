fccabe376dfce833a82aad66ad919592908729d5
Bartosz Sobczynski <baartoszsobczynski@gmail.com>
2015-12-21 12:33:19 +0100

    Build user profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def gravatar_for(user)
+    email = user.email
+    hash = Digest::MD5.hexdigest(email)
+    image_src = "http://www.gravatar.com/avatar/#{hash}"
+    image_tag(image_src, alt: user.name, class: "gravatar")
+  end
 end
